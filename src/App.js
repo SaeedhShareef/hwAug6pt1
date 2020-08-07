05fa09c162cc03e9ed139ac53a39053ff08fa918
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-07 01:01:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PersonList from './Components/PersonList';
+import PersonInput from './Components/PersonInput'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+      <PersonInput />
+      <PersonList/>
+    </div>
+  );
+}
+
+export default App;
